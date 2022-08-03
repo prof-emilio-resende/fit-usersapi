c131f11812a0c09f7e75836a48b343661306e6a1
emilio.resende <emilio.resende@faculdadeimpacta.com.br>
2022-08-02 21:26:01 -0300

    init commit

---
+++ src/test/java/br/edu/impacta/authn/AuthnApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.impacta.authn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
